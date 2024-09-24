88122c3258a2931c128a6248eae31df63973deca
shantanufuke <shantanufuke@gmail.com>
2024-09-24 16:44:39 -0500

    commit

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -23,11 +23,9 @@ const About = ({ ...delegated }: Props) => {
             <div className={styles.aboutme}>
                 <SectionTitle side={"left"}>About Me</SectionTitle>
                 <p className={styles.description}>
-                    Hi there! My name is Shantanu, and I have recently graduated from Arizona State University
-                    with a Masters in Computer Science. As I embark on the next chapter of my career, I am exploring
-                    new opportunities for full-time roles in Full-stack Development, Machine Learning, or
-                    Cloud Infrastructure.
-
+                Hi there! My name is Shantanu Fuke, and I am currently pursuing my Master's in Computer Science at 
+                Texas A&M University. As I embark on the next chapter of my career, I am exploring new opportunities 
+                for full-time roles in Backend Development, SQL Development, Machine Learning, or Data Science.
                 </p>
                 <div className={styles.description}>
                     What I Bring to the Table:<br/>
@@ -79,73 +77,10 @@ const About = ({ ...delegated }: Props) => {
             <picture className={styles.picture}>
                 <source media={MIN_WIDTH.desktop} srcSet={aboutUrl}/>
                 <source media={MIN_WIDTH.tablet} srcSet={aboutUrlSmall}/>
-                <img src={aboutUrl} alt="Shikhar Gupta"/>
+                <img src={aboutUrl} alt="Shantanu Fuke"/>
             </picture>
         </div>
     );
 };
 
 export default About;
-
-
-//
-// export interface IProps {
-//     delegated?: any;
-//     theme: "dark" | "light";
-// }
-//
-// const About: React.FC<React.PropsWithChildren<IProps>> = ({
-//                                                               children,
-//                                                               theme,
-//                                                               ...delegated
-//                                                           }) => {
-//     const downloadResume = () => {
-//         window.open("/resume", "_blank");
-//     };
-//
-//     const aboutUrl =
-//         theme === "light" ? "images/shikhar-3.jpeg" : "images/shikhar-3.jpeg";
-//     return (
-//         <Wrapper {...delegated} id="about">
-//             <AboutMe>
-//                 <SlideUp>
-//                     <SectionTitle label={"About Me "} side={"left"}/>
-//                 </SlideUp>
-//                 <SlideUp>
-//                     <Description>
-//                         Hi there! My name is Shikhar Gupta, and I am currently pursuing a Master's in Computer Science
-//                         at ASU, set to graduate in May 2024.
-//                     </Description>
-//                 </SlideUp>
-//                 <SlideUp>
-//                     <Description>
-//                         I have worked as a Software Engineer at Aruba Networks for 3 years as a full-stack developer,
-//                         and I am well-versed in Python, C++, Docker, Kubernetes, ML frameworks, and Linux along with an
-//                         in-depth understanding of SQL/NoSQL databases software systems.
-//                     </Description>
-//                 </SlideUp>
-//                 <SlideUp>
-//                     <Description>
-//                         Creativity and innovation drive my problem-solving
-//                         approach, as I enjoy building products that positively impact society and automating repetitive
-//                         tasks in my life.
-//                     </Description>
-//                 </SlideUp>
-//                 <SlideUp>
-//                     <Download onClick={downloadResume}>
-//                         Download Resume
-//                     </Download>
-//                 </SlideUp>
-//             </AboutMe>
-//             <Picture>
-//                 <Source media={MIN_WIDTH.desktop} srcSet={aboutUrl}/>
-//                 <Source
-//                     media={MIN_WIDTH.tablet}
-//                     srcSet="images/shikhar-3.jpeg"
-//                 />
-//                 <Image src={aboutUrl} alt="Shikhar Gupta"/>
-//             </Picture>
-//         </Wrapper>
-//     );
-// };
-//
