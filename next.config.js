c10822bdb049572e5b957d5fc4e4365267352ee6
Shantanu Fuke <104629474+shantanufuke@users.noreply.github.com>
2024-09-24 16:15:46 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'opengraph.githubassets.com',
                 port: '',
-                pathname: '/1afeafda/Shikhar97/**',
+                pathname: '/1afeafda/shantanufuke/**',
             },
         ],
     },
