4e316ef9f1ae1614273825271158c97c22998da3
shantanufuke <shantanufuke@gmail.com>
2024-09-24 17:13:24 -0500

    commit

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,6 +3,7 @@ import Button from "@/components/Button";
 import Highlight from "@/components/Highlight/Highlight";
 import SectionTitle from "@/components/SectionTitle/SectionTitle";
 import { MIN_WIDTH } from "breakpoints";
+import styles from "./About.module.css";
 import { useTheme } from "@/context/ThemeContext";
 
 
@@ -45,7 +46,7 @@ const About = ({ ...delegated }: Props) => {
                                 <path
                                     d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            Proficiency in Python, Java, C++, SQL, PL/SQL and frameworks, including ReactJS, Django, Flask, and FastAPI.
+                            Proficiency in Python, Java, C++, SQL, PL/SQL and frameworks, including Django, Flask, and FastAPI.
 
                         </li>
                         <li className="flex items-start">
