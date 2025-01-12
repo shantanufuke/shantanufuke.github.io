d99468968a28adf0565e23adc3a8e0b9482868e0
shantanufuke <shantanufuke@gmail.com>
2025-01-12 12:10:50 +0530

    commit

--- src/components/Experiences/Experiences.tsx
+++ src/components/Experiences/Experiences.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Experiences: React.FC = () => {
   return (
-    <div className={styles.experiences}>
+    <div className={styles.experiences} id="experiences">
       <MaxWidthWrapper>
         <ConstrainedTitle side="left">Experiences</ConstrainedTitle>
         <div className={styles.items}>
@@ -34,11 +34,11 @@ const Experiences: React.FC = () => {
               <h4 className={styles.company}>Texas A&M University</h4>
               <h4 className={styles.role}>Research Assistant</h4>
               <p className={styles.description}>
-                    ⦿ &nbsp; &nbsp; Designed and implemented neural networks for DNA enhancer prediction, achieving a 20% increase in prediction accuracy.<br/><br/>
-                    ⦿ &nbsp; &nbsp; Developed a C++ tool to determine sequence similarity, reducing computation time by 40% and increasing similarity
+                    ⦿&nbsp;Designed and implemented neural networks for DNA enhancer prediction, achieving a 20% increase in prediction accuracy.<br/><br/>
+                    ⦿&nbsp;Developed a C++ tool to determine sequence similarity, reducing computation time by 40% and increasing similarity
                     measurement accuracy by 10%.<br/><br/>
-                    ⦿ &nbsp; &nbsp; Automated the data pipeline using Python, reducing manual processing time by 40%.<br/><br/>
-                    ⦿ &nbsp; &nbsp; Utilized Python for data preprocessing, transforming large FASTA files into neural network-compatible formats, boosting model
+                    ⦿&nbsp;Automated the data pipeline using Python, reducing manual processing time by 40%.<br/><br/>
+                    ⦿&nbsp;Utilized Python for data preprocessing, transforming large FASTA files into neural network-compatible formats, boosting model
                     accuracy by 15%.<br/>
               </p>
             </div>
@@ -70,10 +70,10 @@ const Experiences: React.FC = () => {
                   <h4 className={styles.company}>Deloitte Consulting</h4>
                   <h4 className={styles.role}>Oracle PL/SQL Developer</h4>
                   <p className={styles.description}>
-                    ⦿ &nbsp; &nbsp; Developed the "Deals to Stock Ledger Interface" in PL/SQL for 
+                    ⦿&nbsp;Developed the "Deals to Stock Ledger Interface" in PL/SQL for 
                     Oracle Retail Merchandising Suite, ensuring efficient deal processing and data integrity 
                     in the stock ledger.<br/><br/>
-                    ⦿ &nbsp; &nbsp; Optimized data transfer from Google Cloud Platform (GCP) to Oracle RMS with an 
+                    ⦿&nbsp;Optimized data transfer from Google Cloud Platform (GCP) to Oracle RMS with an 
                     automated PL/SQL program, reducing upload time by 40% and improving scalability.<br/>
                   </p>
                 </div>
@@ -86,9 +86,9 @@ const Experiences: React.FC = () => {
                   {/* <p className={styles.time}>January 2022 - June 2022</p> */}
                   <h4 className={styles.role}>Oracle Retail Developer</h4>
                   <p className={styles.description}>
-                    ⦿ &nbsp; &nbsp; Integrated Oracle RMS with SAP ERP to automate invoice processing, 
+                    ⦿&nbsp;Integrated Oracle RMS with SAP ERP to automate invoice processing, 
                     improving workflow efficiency by 60% and enhancing data accuracy.<br/><br/>
-                    ⦿ &nbsp; &nbsp; Led the design of an Oracle Retail Cloud location list creation program, 
+                    ⦿&nbsp;Led the design of an Oracle Retail Cloud location list creation program, 
                     reducing user time by 70% and improving team performance.<br/>
                   </p>
                 </div>
@@ -103,8 +103,10 @@ const Experiences: React.FC = () => {
                   <h4 className={styles.role}>Oracle BI Developer</h4>
 
                   <p className={styles.description}>
-                    ⦿ &nbsp; &nbsp; Developed Oracle BI reports, utilizing SQL to extract data and 
-                    generate comprehensive reports, improving accuracy and operational efficiency.<br/>
+                    ⦿&nbsp;Developed Oracle BI reports, utilizing SQL to extract data and 
+                    generate comprehensive reports, improving accuracy and operational efficiency.<br/><br/>
+                    ⦿&nbsp;Worked on optimizing complex SQL queries in BI reports to enhance performance 
+                    and reduce query execution time, ensuring seamless data delivery.<br/>
                   </p>
                 </div>
               </div>
@@ -136,11 +138,11 @@ const Experiences: React.FC = () => {
                 Oracle SQL Developer
               </h4>
               <p className={styles.description}>
-                  ⦿ &nbsp; &nbsp; Automated 80% of Coupa purchase orders into Oracle E-Business Suite using PL/SQL, reducing manual entry by 20 hours per week.<br/><br/>
-                  ⦿ &nbsp; &nbsp; Streamlined supplier invoice management in Oracle EBS, improving data accuracy by 80% and reducing processing time by 60%.<br/><br/>
-                  ⦿ &nbsp; &nbsp; Cut account creation time by 60% through custom PL/SQL automation for user creation and email notifications.<br/><br/>
-                  ⦿ &nbsp; &nbsp; Automated supplier attribute updates using Oracle EBS Supplier API, reducing manual efforts by 80% and improving scalability.<br/><br/>
-                  ⦿ &nbsp; &nbsp; Designed efficient data models and managed schemas for Oracle EBS integration with optimized DDLs.<br/><br/>
+                  ⦿&nbsp;Automated 80% of Coupa purchase orders into Oracle E-Business Suite using PL/SQL, reducing manual entry by 20 hours per week.<br/><br/>
+                  ⦿&nbsp;Streamlined supplier invoice management in Oracle EBS, improving data accuracy by 80% and reducing processing time by 60%.<br/><br/>
+                  ⦿&nbsp;Cut account creation time by 60% through custom PL/SQL automation for user creation and email notifications.<br/><br/>
+                  ⦿&nbsp;Automated supplier attribute updates using Oracle EBS Supplier API, reducing manual efforts by 80% and improving scalability.<br/><br/>
+                  ⦿&nbsp;Designed efficient data models and managed schemas for Oracle EBS integration with optimized DDLs.<br/><br/>
               </p>
             </div>
           </div>
